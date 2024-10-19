d31b53eada3ef3a703899b6e9c0af2d5d0768371
wilitp <guilledeipola@gmail.com>
2024-10-19 20:12:04 -0300

    Fix custom items

--- custom.js
+++ custom.js
@@ -2,20 +2,16 @@
 // Identificar custom items usados en el latex
 
 const customItems = Array.from(document.querySelectorAll("li")).filter(
-  li => li.textContent?.includes("adhocprefix")
+  li => li.innerHTML?.includes("adhocprefix")
 )
 
 for (const customItem of customItems) {
 
-  // obtener el item entre los delimitadores adhoc
-  const itemText = customItem.textContent?.match(/adhocprefix(.*)adhocsufix/, "")[1]?.trim() ?? ""
-
   // quitar el texto que identifica al custom item
-  customItem.textContent = customItem.textContent?.replace(/adhocprefix.*adhocsufix/, "") ?? ""
+  //customItem.innerHTML = customItem.innerHTML?.replace(/adhocprefix.*adhocsufix/, "") ?? ""
 
   // quitar marker original
   customItem.classList.add("no-marker");
-
-  // agregar item custom al principio, como si fuera el marker
-  customItem.textContent = itemText + " " + customItem.textContent
 }
+
+document.body.innerHTML = document.body.innerHTML.replaceAll(/adhocprefix|adhocsufix/g, "")
